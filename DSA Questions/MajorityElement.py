ff56dc08b5e786cb09592590d15632d0d9355991
kbir079 <116657420+kbir-dev@users.noreply.github.com>
2025-01-14 13:30:18 +0530

    day 5

---
+++ DSA Questions/MajorityElement.py
@@ -0,0 +1,8 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        counts = {}
+        for num in nums:
+            counts[num] = counts.get(num, 0) + 1
+            if counts[num] > len(nums) // 2:
+                return num
+        
