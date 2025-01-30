f9180575d791267674cfa1daffada2be3ea50bc6
kbir079 <116657420+kbir-dev@users.noreply.github.com>
2025-01-30 22:22:35 +0530

    Create SearchInsertPosition.py

---
+++ DSA Questions/SearchInsertPosition.py
@@ -0,0 +1,14 @@
+from typing import List
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        s, e = 0, len(nums) - 1
+        while s <= e:
+            mid = (s + e) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                s = mid + 1
+            else:
+                e = mid - 1
+        return s  
